8cecd7a9134fcc13dc3055bb2d31bf25a507f142
Oleksandr Kozachuk <oleksandr.kozachuk@exasol.com>
2017-04-07 13:15:22 +0200

    Better basic_test implementation.

--- basic_test.sh
+++ basic_test.sh
@@ -1,20 +1,32 @@
 #! /bin/bash
 
+BINARY="./exadt"
+if (( $# > 0 )); then
+    BINARY="$1"
+fi
+
+ROOT="$HOME"
+if (( $# > 1 )); then
+    ROOT="$2"
+fi
+
 echo "=== Starting exadt basic test ==="
 set -x &&
-./exadt create-cluster --root ~/MyCluster/ --create-root MyCluster &&
-./exadt list-clusters &&
-./exadt init-cluster --image exasol/docker-db:6.0.0-d1 --license ./license.xml --device-type file --auto-storage --force MyCluster &&
-./exadt start-cluster MyCluster &&
-./exadt ps MyCluster &&
-./exadt stop-cluster MyCluster &&
-./exadt create-file-devices --size 10GiB MyCluster &&
-./exadt create-file-devices --size 10GiB MyCluster --replace --path $HOME &&
-./exadt update-cluster --image exasol/docker-db:latest MyCluster &&
-./exadt start-cluster MyCluster &&
-./exadt stop-cluster MyCluster &&
-./exadt start-cluster MyCluster --command "/bin/sleep 30" &&
-./exadt stop-cluster MyCluster --timeout 2 &&
-./exadt delete-cluster MyCluster &&
+"$BINARY" list-clusters &&
+"$BINARY" create-cluster --root "$ROOT/MyCluster/" --create-root MyCluster &&
+"$BINARY" list-clusters &&
+"$BINARY" init-cluster --image exasol/docker-db:6.0.0-d1 --license ./license.xml --device-type file --auto-storage --force MyCluster &&
+"$BINARY" list-clusters &&
+"$BINARY" start-cluster MyCluster &&
+"$BINARY" ps MyCluster &&
+"$BINARY" stop-cluster MyCluster &&
+"$BINARY" create-file-devices --size 10GiB MyCluster &&
+yes | "$BINARY" create-file-devices --size 10GiB MyCluster --replace --path $HOME &&
+"$BINARY" update-cluster --image exasol/docker-db:latest MyCluster &&
+"$BINARY" start-cluster MyCluster &&
+"$BINARY" stop-cluster MyCluster &&
+"$BINARY" start-cluster MyCluster --command "/bin/sleep 30" &&
+"$BINARY" stop-cluster MyCluster --timeout 2 &&
+yes | "$BINARY" delete-cluster MyCluster &&
 set +x &&
 echo "=== Successful! ==="
