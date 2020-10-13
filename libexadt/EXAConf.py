cfa74bb169b8fd66188fe21deada48e8a54cb842
Christian Keller <christian.keller@exasol.com>
2020-10-13 09:16:26 +0200

    Automated commit (tag: '6.1.13-d1')

--- libexadt/EXAConf.py
+++ libexadt/EXAConf.py
@@ -195,8 +195,8 @@ class EXAConf:
         # or taken from the Docker image).
         # The 'version' parameter is static and denotes the version
         # of the EXAConf python module and EXAConf format
-        self.version = "6.1.12"
-        self.re_version = "6.1.12"
+        self.version = "6.1.13"
+        self.re_version = "6.1.13"
         self.set_os_version(self.version)
         self.set_db_version(self.version)
         self.set_re_version(self.re_version)
